4f64ec027cc4439a88848604a445b1050c23c7e7
shimozonomisato <zazizuzezono@gmail.com>
2017-09-28 19:42:12 +0900

    判定できるようになった

--- janken.php
+++ janken.php
@@ -1,28 +1,69 @@
 <?php
-//$playerと$comが等しければあいこ
-//$player + 1 を3で割った余りと$comが等しければ$comの勝ち
-//それ以外は$meの勝ち
 
-$choice = $_POST ['choice'];
+echo ("途中経過1-0[".$_POST["choice"]."]<br>");
+$choice = $_POST['choice'];
+echo ("途中経過1-1[".$choice."]<br>");
 
-function janken($player, $com) {
-    if ($player == $com)
-    echo $result[0];
-  } elseif (($player +1 %3) == $com) {
-    echo $result[2];
-  } else {
-    echo $result[1]:
-  }
+if ($choice == "グー") {
+   echo ("途中経過1-21<br>");
+    echo $choice_temp = 0;
+  } elseif ($choice == "チョキ") {
+    echo ("途中経過1-22<br>");
+    echo $choice_temp = 1;
+  } elseif ($choice == "パー") {
+    echo ("途中経過1-23<br>");
+    echo $choice_temp = 2;
+  }else{
+    echo ("途中経過1-24<br>");
+    echo 'X';
 }
 
-//結果の配列
-$result = array('あいこです', 'あなたの勝ちです', 'あなたの負けです',);
+echo '自分'.$choice_temp;
 
-//出し手の配列,0=>グー、1=>チョキ、2=>パーを代入
-$hand = array('グー', 'チョキ', 'パー');
+echo ("途中経過1[".$choice."]<br>");
+echo $choice."<br>";
+var_dump($choice_temp)."<br>";
 
-//$comの出し手
-$com = array_rand($hand);
+// 0=>グー、1=>チョキ、2=>パーを代入
+$hand = array('グー','チョキ','パー');
+echo ("途中経過1-2<br>");
 
-//$playerの出し手
-$player = $hand[];
+
+
+echo ("途中経過1-3<br>");
+
+
+//自分の手
+//$player = $hand[];
+
+//コンピューターの手
+$com =array_rand($hand);
+janken($choice_temp,$com);
+echo ("おしまい<br>");
+echo ("コンピュータ[".$com."]<br>");
+//$com反映されてる
+
+
+//--------------------------
+//じゃんけんプログラム
+//$playerと$comが等しければあいこ
+//$player + 1 を3で割った余りと$comが等しければ$comの勝ち
+//それ以外は$playerの勝ち
+//--------------------------
+function janken($choice_f, $com_f) {
+  //結果
+  $result = array('あいこです<br>','あなたの勝ちです<br>','あなたの負けです<br>' , '判定不能<br>');
+
+  echo ("途中経過2-01[".$choice_f."]<br>");
+  echo ("途中経過2-02[".$com_f."]<br>");
+  if ($choice_f == $com_f){
+    echo ("途中経過2<br>");
+    echo $result[0];
+ } elseif ((($choice_f +1) %3) == $com_f) {
+   echo ("途中経過4<br>");
+   echo $result[1];
+  } else {
+    echo ("途中経過3<br>");
+    echo $result[2];
+  }
+}
