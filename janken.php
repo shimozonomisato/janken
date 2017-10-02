dfe19faefbde4ed5211560f086d78bc3298bfdf0
shimozonomisato <zazizuzezono@gmail.com>
2017-10-02 15:27:37 +0900

    双方の出し手の表示を追加

--- janken.php
+++ janken.php
@@ -1,9 +1,15 @@
 <?php
+//エスケープ
+//function h($s) {
+//  return htmlspecislchars($s, ENT_QUOTES, 'UTF-8');
+//}
+//h($choice);
 
 echo ("途中経過1-0[".$_POST["choice"]."]<br>");
 $choice = $_POST['choice'];
 echo ("途中経過1-1[".$choice."]<br>");
 
+//プレイヤーの手を配列に入れる
 if ($choice == "グー") {
    echo ("途中経過1-21<br>");
     echo $choice_temp = 0;
@@ -33,15 +39,31 @@ echo ("途中経過1-2<br>");
 echo ("途中経過1-3<br>");
 
 
-//自分の手
-//$player = $hand[];
-
 //コンピューターの手
 $com =array_rand($hand);
+
+if ($com == 0) {
+    echo $com_t = 'グー';
+  } elseif ($com == 1 ) {
+    echo $com_t = 'チョキ';
+  } elseif ($com == 2 ) {
+    echo $com_t = 'パー';
+  }
+
 janken($choice_temp,$com);
-echo ("おしまい<br>");
-echo ("コンピュータ[".$com."]<br>");
-//$com反映されてる
+
+//結果表示
+echo 'あなたは ['. $choice. '] を出しました。'. "<br>";
+
+if ($choice == $com_t) {
+    echo 'コンピュータも ['. $com_t. '] を出しました。'. "<br>";
+  } else {
+    echo 'コンピュータは ['. $com_t. '] を出しました。'. "<br>";
+}
+
+echo 'おしまい'."<br>";
+
+
 
 
 //--------------------------
@@ -52,7 +74,7 @@ echo ("コンピュータ[".$com."]<br>");
 //--------------------------
 function janken($choice_f, $com_f) {
   //結果
-  $result = array('あいこです<br>','あなたの勝ちです<br>','あなたの負けです<br>' , '判定不能<br>');
+  $result = array('あいこです<br>','あなたの勝ちです<br>','あなたの負けです<br>');
 
   echo ("途中経過2-01[".$choice_f."]<br>");
   echo ("途中経過2-02[".$com_f."]<br>");
